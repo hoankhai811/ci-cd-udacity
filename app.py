b56fd16d7bac9715ef583a0422b70e24d1f22f0a
Tri Nguyen Trung <nttri255@gmail.com>
2023-05-02 15:45:45 +0700

    change title app

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = "<h3>Sklearn Prediction Home CI CD Applied Test Successfully Demo</h3>"
+    html = "<h3>Sklearn Prediction Home</h3>"
     return html.format(format)
 
 # TO DO:  Log out the prediction value
